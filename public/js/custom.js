16c5b723b0a6e4b4c32fe83374187d391b902ff1
Rohan Saxena <freak.rhn@gmail.com>
2020-07-21 21:00:01 +0530

    Charts functions

--- public/js/custom.js
+++ public/js/custom.js
@@ -21,7 +21,14 @@ render_chart = function(container_id, title, chart_type, unit_name, Yaxis_title,
     chart.render();
 }
 
-render_chart_bp = function(container_id, title, chart_type, unit_name, Yaxis_title, data) {
+render_chart_bp = function(container_id, title, unit_name, Yaxis_title, data) {
+    data_low = [];
+    data_high = [];
+    var i;
+    for (i = 0; i < cars.length; i++) {
+        data_low.add({ "y": data[i]["y2"], "x": data[i]["x"] });
+        data_high.add({ "y": data[i]["y1"], "x": data[i]["x"] });
+    }
     var chart = new CanvasJS.Chart(container_id, {
         title: {
             text: title
@@ -33,12 +40,20 @@ render_chart_bp = function(container_id, title, chart_type, unit_name, Yaxis_tit
         },
         data: [{
             toolTipContent: "{y} " + unit_name.toString(),
-            type: chart_type,
+            type: "line",
             showInLegend: true,
             // legendText: "source: Nielsen SoundScan",
             markerSize: 5,
-            color: "rgba(54,158,173,.7)",
-            dataPoints: data
+            color: "red",
+            dataPoints: data_high
+        }, {
+            toolTipContent: "{y} " + unit_name.toString(),
+            type: "line",
+            showInLegend: true,
+            // legendText: "source: Nielsen SoundScan",
+            markerSize: 5,
+            color: "blue",
+            dataPoints: data_low
         }]
     });
     chart.render();
