7f27a61b888e52af2219e7ebab12baf72b8737c3
rakshak1010 <rakshak.malhotra48@gmail.com>
2020-07-21 21:41:33 +0530

    add ecg and temperature graph

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,4 +1,65 @@
-render_chart = function(container_id, title, chart_type, unit_name, Yaxis_title, data) {
+render_chart = function (container_id, title, chart_type, unit_name, Yaxis_title, data, attribute) {
+    var dummy = [{
+        x: new Date("July 1, 2020 11:13:00"),
+        y: 100
+    }, {
+        x: new Date("July 2, 2020 11:13:00"),
+        y: 99.4
+    }, {
+        x: new Date("July 3, 2020 11:13:00"),
+        y: 99.2,
+    }, {
+        x: new Date("July 4, 2020 11:13:00"),
+        y: 104,
+        indexLabel: "highest"
+    }, {
+        x: new Date("July 5, 2020 11:13:00"),
+        y: 102.5
+    }, {
+        x: new Date("July 6, 2020 11:13:00"),
+        y: 100.1,
+    }, {
+        x: new Date("July 7, 2020 11:13:00"),
+        y: 99
+    }, {
+        x: new Date("July 8, 2020 11:13:00"),
+        y: 98.6
+    }, {
+        x: new Date("July 9, 2020 11:13:00"),
+        y: 99,
+    }, {
+        x: new Date("July 10, 2020 11:13:00"),
+        y: 102.5
+    }, {
+        x: new Date("July 11, 2020 11:13:00"),
+        y: 100.1,
+    }, {
+        x: new Date("July 12, 2020 11:13:00"),
+        y: 99
+    }, {
+        x: new Date("July 13, 2020 11:13:00"),
+        y: 98.6
+    }, {
+        x: new Date("July 14, 2020 11:13:00"),
+        y: 99,
+    }, {
+        x: new Date("July 15, 2020 11:13:00"),
+        y: 102.5
+    }, {
+        x: new Date("July 16, 2020 11:13:00"),
+        y: 100.1,
+    }, {
+        x: new Date("July 17, 2020 11:13:00"),
+        y: 99
+    }, {
+        x: new Date("July 18, 2020 11:13:00"),
+        y: 98.6
+    }, {
+        x: new Date("July 19, 2020 11:13:00"),
+        y: 99,
+    }
+    ];
+
     var chart = new CanvasJS.Chart(container_id, {
         title: {
             text: title
@@ -15,13 +76,14 @@ render_chart = function(container_id, title, chart_type, unit_name, Yaxis_title,
             // legendText: "source: Nielsen SoundScan",
             markerSize: 5,
             color: "rgba(54,158,173,.7)",
-            dataPoints: data
+            dataPoints: dummy
+            // dataPoints: data[attribute]
         }]
     });
     chart.render();
 }
 
-render_chart_bp = function(container_id, title, unit_name, Yaxis_title, data) {
+render_chart_bp = function (container_id, title, unit_name, Yaxis_title, data, attribute) {
     data_low = [];
     data_high = [];
     var i;
