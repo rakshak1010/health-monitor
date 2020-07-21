76f32505e17e5f5286ce11a4c88d564bb1efd32b
rakshak1010 <rakshak.malhotra48@gmail.com>
2020-07-21 19:35:58 +0530

    add health model

---
+++ models/health.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const HealthSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        bp: [
+            {
+                y1: Number,
+                y2: Number,
+                x: Date
+            },
+        ],
+        ecg: [
+            {
+                y: Number,
+                x: Date
+            },
+        ],
+        spo2: [
+            {
+                y: Number,
+                x: Date
+            },
+        ],
+        temperature: [
+            {
+                y: Number,
+                x: Date
+            },
+        ]
+    },
+    {
+        id: true,
+        timestamps: true
+    }
+);
+
+
+Health = mongoose.model("Health", HealthSchema);
+
+module.exports = Health;
